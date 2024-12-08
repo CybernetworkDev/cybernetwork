62dc388375a4667ef1595c25e59fe38b233ce64c
yusrililhm <yusrililham62@gmail.com>
2024-12-08 08:20:04 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export const content = ["./*.html"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [daisyui];
+
